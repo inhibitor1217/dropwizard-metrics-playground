c52322610906ed8c02120c64e1d33e081818ded9
dugi <dugi@channel.io>
2022-12-23 17:12:09 +0900

    feat(services): register Slf4jReporter to log metrics

--- src/main/java/io/inhibitor/services/MetricsManager.java
+++ src/main/java/io/inhibitor/services/MetricsManager.java
@@ -1,18 +1,24 @@
 package io.inhibitor.services;
 
-import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.ScheduledReporter;
+import com.codahale.metrics.Slf4jReporter;
+import com.codahale.metrics.Slf4jReporter.LoggingLevel;
 import io.dropwizard.lifecycle.Managed;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MetricsManager implements Managed {
 
-  private final ConsoleReporter consoleReporter;
+  private final ScheduledReporter reporter;
 
   @Inject
   public MetricsManager(MetricRegistry metricRegistry) {
-    this.consoleReporter = ConsoleReporter.forRegistry(metricRegistry)
+    this.reporter = Slf4jReporter.forRegistry(metricRegistry)
+        .outputTo(log)
+        .withLoggingLevel(LoggingLevel.INFO)
         .convertRatesTo(TimeUnit.SECONDS)
         .convertDurationsTo(TimeUnit.MILLISECONDS)
         .build();
@@ -20,11 +26,11 @@ public class MetricsManager implements Managed {
 
   @Override
   public void start() {
-    consoleReporter.start(1, TimeUnit.SECONDS);
+    reporter.start(10, TimeUnit.SECONDS);
   }
 
   @Override
   public void stop() {
-    consoleReporter.stop();
+    reporter.stop();
   }
 }
